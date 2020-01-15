01585d20bedea04420f84a18a41db3f52721f5ff
swyx <shawnthe1@gmail.com>
2020-01-14 20:59:44 -0500

    init

---
+++ storybook-static/main.e91a4b5abeaf8a52aef0.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(467),p(657),p(1331),p(1333),n.exports=p(1364)},function(n,o){}],[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.f37a3d3f9f6db9b9502341df7a27f27c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895995f2bf8bce70ac2fcd01734736fb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.71709ac8382d769963b4.bundle.js"
+  },
+  {
+    "url": "runtime~main.71709ac8382d769963b4.bundle.js"
+  },
+  {
+    "url": "vendors~main.71709ac8382d769963b4.bundle.js"
+  },
+  {
+    "url": "vendors~main.71709ac8382d769963b4.bundle.js.LICENSE"
+  }
+]);
